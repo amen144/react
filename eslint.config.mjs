302d30255b1e02aef7a3d5ec57fb12016050cc6f
amen144 <amenjaballi08@gmail.com>
2025-10-31 23:05:46 +0100

    Updated API routes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,6 @@ export default defineConfig([
   { ignores: ["**/*.js", "**/*.cjs", "**/*.mjs"] },
   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
   
-{extends: ["next/core-web-vitals", "eslint:recommended", "plugin:@typescript-eslint/recommended"]},
 {rules: {
     "@typescript-eslint/no-explicit-any": "off"
   }
