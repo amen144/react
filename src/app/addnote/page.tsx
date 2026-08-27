ad395771dd60e996e33de3bd518389c4b261bf0c
Amen Allah Jaballi <amenjaballi08@gmail.com>
2026-08-27 14:21:41 +0100

    Remove import of AudioControls component

--- src/app/addnote/page.tsx
+++ src/app/addnote/page.tsx
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import AudioControls from "../component/audioCtrl";
 export default function AddNote() {
   const router = useRouter();
   const [title, setTitle] = useState("");
